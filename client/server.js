164b8cd6d1bf028e9f5cc5ad9c41c8c50f7acad7
Isabel Cristina <isabel.cristina@dmz.org.br>
2023-11-04 07:52:05 -0300

    remove axios add fecth

--- client/server.js
+++ client/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const axios = require("axios");
 const app = express();
 const port = 3000;
 
@@ -11,18 +10,16 @@ app.get("/", function (req, res) {
   res.render("pages/home");
 });
 
-app.get("/pets", function (req, res) {
+app.get("/pets", async function (req, res) {
   const page = req.query.page ? req.query.page - 1 : 0;
-  axios
-    .get(`http://localhost:3001/pets?page=${page}&size=9`)
-    .then((response) => {
-      const data = response.data;
-      res.render("pages/pets", { data: data.items, total: data.total });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("Erro ao obter dados da API");
-    });
+  try {
+    const response = await fetch(`http://localhost:3001/pets?page=${page}&size=9`)
+    const data = await response.json();
+    res.render("pages/pets", { data: data.items, total: data.total });
+  } catch(error) {
+    console.error(error);
+    res.status(500).send("Erro ao obter dados da API");
+  }
 });
 
 app.listen(port, () => {
