ceb8f1306f6b99aaa254d855acf3a0468d6782f1
Isabel Cristina <isabel.cristina@dmz.org.br>
2023-11-05 14:13:13 -0300

    menu para user

--- client/server.js
+++ client/server.js
@@ -18,24 +18,30 @@ const user = {
 };
 
 app.get("/", (req, res) => { 
-  res.cookie("user", user);
-  res.render("pages/home") 
+  res.render("pages/home", { user: req.cookies.user }) 
 });
+
+app.get("/login", (req, res) => { 
+  const name = user.name.split(' ');
+  res.cookie("user", { name: `${name[0]} ${name[name.length - 1]}`, role: user.role });
+  res.render("pages/login") 
+});
+
 
 app.get("/pets", async (req, res) => {
   const page = req.query.page ? req.query.page : 1;
   try {
     const response = await fetch(`http://localhost:3001/pets?page=${page}&size=9`);
     const data = await response.json();
-    res.render("pages/pets", { data: data.items, total: data.total, current_page: 'pets', size: 9, page: page });
+    res.render("pages/pets", { data: data.items, total: data.total, current_page: 'pets', size: 9, page: page, user: req.cookies.user });
   } catch (error) {
-    res.render("pages/pets", { data: [], total: 0, message: "Internal Server Error" });
+    res.render("pages/pets", { data: [], total: 0, message: "Internal Server Error", user: req.cookies.user });
   }
 });
 
 app.get("/admin", (req, res, next) => auth(req, res, next, {allowed: ["ADMIN"]}),
   (req, res) => {
-    res.render("pages/admin");
+    res.render("pages/admin", { user: req.cookies.user });
   }
 );
 
@@ -45,9 +51,9 @@ app.get("/admin/users", (req, res, next) => auth(req, res, next, {allowed: ["ADM
     try {
       const response = await fetch(`http://localhost:3001/users?page=${page}&size=10`);
       const data = await response.json();
-      res.render("pages/admin/users", { data: data.items, total: data.total, current_page: 'users', size: 10, page: page });
+      res.render("pages/admin/users", { data: data.items, total: data.total, current_page: 'users', size: 10, page: page, user: req.cookies.user });
     } catch (error) {
-      res.render("pages/admin/users", { data: [], total: 0, message: "Internal Server Error" });
+      res.render("pages/admin/users", { data: [], total: 0, message: "Internal Server Error", user: req.cookies.user });
     }
   }
 );
@@ -58,9 +64,9 @@ app.get("/admin/pets", (req, res, next) => auth(req, res, next, {allowed: ["ADMI
     try {
       const response = await fetch(`http://localhost:3001/pets?page=${page}&size=10`);
       const data = await response.json();
-      res.render("pages/admin/pets", { data: data.items, total: data.total, current_page: 'pets', size: 10, page: page });
+      res.render("pages/admin/pets", { data: data.items, total: data.total, current_page: 'pets', size: 10, page: page, user: req.cookies.user });
     } catch (error) {
-      res.render("pages/admin/pets", { data: [], total: 0, message: "Internal Server Error" });
+      res.render("pages/admin/pets", { data: [], total: 0, message: "Internal Server Error", user: req.cookies.user });
     }
   }
 );
@@ -71,9 +77,9 @@ app.get("/admin/solicitations", (req, res, next) => auth(req, res, next, {allowe
     try {
       const response = await fetch(`http://localhost:3001/solicitations?page=${page}&size=10`);
       const data = await response.json();
-      res.render("pages/admin/solicitations", { data: data.items, total: data.total, current_page: 'pets', size: 10, page: page });
+      res.render("pages/admin/solicitations", { data: data.items, total: data.total, current_page: 'pets', size: 10, page: page, user: req.cookies.user });
     } catch (error) {
-      res.render("pages/admin/solicitations", { data: [], total: 0, message: "Internal Server Error" });
+      res.render("pages/admin/solicitations", { data: [], total: 0, message: "Internal Server Error", user: req.cookies.user });
     }
   }
 );
