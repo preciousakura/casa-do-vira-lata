35e20eb4e4f7082dae5e4999f8019835abc9923c
Isabel Cristina <isabel.cristina@dmz.org.br>
2023-11-07 15:28:04 -0300

    carrossel responsivo finalizado

--- client/public/js/carousel.js
+++ client/public/js/carousel.js
@@ -5,7 +5,7 @@ let currentIndex = 0;
 const size = slides.length;
 
 function nextSlide() {
-  if (currentIndex + 1 === size - 1) currentIndex = 0;
+  if (currentIndex + 1 === size - 1 && window.innerWidth > 1112) currentIndex = 0;
   else currentIndex = (currentIndex + 1) % slides.length;
   updateCarousel();
 }
@@ -16,8 +16,12 @@ function prevSlide() {
 }
 
 function updateCarousel() {
-  const offset = -currentIndex * (100 / 2) + "%";
+  let offset;
+  if (window.innerWidth <= 1112) offset = -currentIndex * 100 + "%";
+  else offset = -currentIndex * 50 + "%";
   carouselContainer.style.transform = `translateX(${offset})`;
 }
+
+
 
 setInterval(nextSlide, 10000);
