ac4abf1a2a43a8ed4a568d4b2882d41be01b7bba
Isabel Cristina <isabel.cristina@dmz.org.br>
2023-11-07 15:42:18 -0300

    response slider

--- client/public/js/carousel.js
+++ client/public/js/carousel.js
@@ -2,20 +2,24 @@ const carouselContainer = document.querySelector(".carousel-inner");
 const slides = document.querySelectorAll(".carousel-slide");
 
 let currentIndex = 0;
-const size = Math.ceil(slides.length/2);
+const size = slides.length;
 
-function prevSlide() {
-  currentIndex = (currentIndex + 2) % size;
+function nextSlide() {
+  if (currentIndex + 1 === size - 1 && window.innerWidth > 1112)
+    currentIndex = 0;
+  else currentIndex = (currentIndex + 1) % slides.length;
   updateCarousel();
 }
 
-function nextSlide() {
-  currentIndex = (currentIndex - 2 + size) % size;
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
   updateCarousel();
 }
 
 function updateCarousel() {
-  const offset = -currentIndex * 100 + "%";
+  let offset;
+  if (window.innerWidth <= 1112) offset = -currentIndex * 100 + "%";
+  else offset = -currentIndex * 50 + "%";
   carouselContainer.style.transform = `translateX(${offset})`;
 }
 
