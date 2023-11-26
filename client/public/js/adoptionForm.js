c0307fe4bf4b523b3153e5ad36cf9cfd9b5227b0
Isabel Cristina <isabel.cristina@dmz.org.br>
2023-11-26 19:15:06 -0300

    adopt pet form

--- client/public/js/adoptionForm.js
+++ client/public/js/adoptionForm.js
@@ -1,28 +1,47 @@
-// adoptionForm.js
-document.addEventListener("DOMContentLoaded", (event) => {
-  const form = document.getElementById("adoptionForm");
-  const successModal = document.getElementById("successModal");
+// // adoptionForm.js
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   const form = document.getElementById("adoptionForm");
+//   const successModal = document.getElementById("successModal");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    openModal("modal-adoption");
-    form.reset(); 
-    const formData = new FormData(this);
-    fetch("/submit-adoption-form", {
-      method: "POST",
-      body: formData,
+//   form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     openModal("modal-adoption");
+//     form.reset(); 
+//     const formData = new FormData(this);
+//     fetch("/submit-adoption-form", {
+//       method: "POST",
+//       body: formData,
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         successModal.style.display = "block";
+//       })
+//       .catch((error) => {
+//         console.error(
+//           "Houve um problema com a operação fetch: " + error.message
+//         );
+//       });
+//   });
+// });
+
+async function adoptMe(e) {
+  e.preventDefault();
+  const data = new FormData(e.target);
+
+  try {
+    const res = await fetch('http://localhost:3001/pets/adopt', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(Object.fromEntries(data.entries())),
     })
-      .then((response) => response.json())
-      .then((data) => {
-        successModal.style.display = "block";
-      })
-      .catch((error) => {
-        console.error(
-          "Houve um problema com a operação fetch: " + error.message
-        );
-      });
-  });
-});
+    if(res.ok) alert("Solicitação de adoção realizada com sucesso!")
+    else alert("Erro ao solicitar animal")
+  } catch (err) {
+      alert("Erro ao solicitar animal")
+  }
+}
 
 function redirectToUserDefault() {
   window.location.href = "/user-default";
--- client/public/js/user-default.js
+++ client/public/js/user-default.js
@@ -74,6 +74,7 @@ function removeFavoriteFromLocalStorage(petId) {
   favorites = favorites.filter(favId => favId !== petId);
   localStorage.setItem("favorites", JSON.stringify(favorites));
 }
+
 function toggleFavoriteIcon(petId) {
   const favoriteIcons = document.querySelectorAll(`i[onclick='addToFavorites(${petId})']`);
   favoriteIcons.forEach((icon) => {
@@ -81,61 +82,60 @@ function toggleFavoriteIcon(petId) {
   });
 }
 
-
 function getFavoritesFromLocalStorage() {
   const favorites = localStorage.getItem("favorites");
   return favorites ? JSON.parse(favorites) : [];
 }
 
-function applyFilters() {
-  const type = document.querySelector(".filters__select__type").value;
-  const size = document.querySelector(".filters__select__size").value;
-  const gender = document.querySelector(".filters__select__gender").value;
-  const name = document.getElementById("filterSearch").value;
-  const castrated = document.getElementById("castrated").checked;
-  const vaccinated = document.getElementById("vaccinated").checked;
-  const dewormed = document.getElementById("dewormed").checked;
-  fetch(
-    `${backendUrl}/petsFilter?type=${type}&size=${size}&gender=${gender}&name=${name}&castrated=${castrated}&vaccinated=${vaccinated}&dewormed=${dewormed}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      updatePetList(data);
-    })
-    .catch((error) => console.error("Erro ao aplicar filtros:", error));
-}
+// function applyFilters() {
+//   const type = document.querySelector(".filters__select__type").value;
+//   const size = document.querySelector(".filters__select__size").value;
+//   const gender = document.querySelector(".filters__select__gender").value;
+//   const name = document.getElementById("filterSearch").value;
+//   const castrated = document.getElementById("castrated").checked;
+//   const vaccinated = document.getElementById("vaccinated").checked;
+//   const dewormed = document.getElementById("dewormed").checked;
+//   fetch(
+//     `${backendUrl}/petsFilter?type=${type}&size=${size}&gender=${gender}&name=${name}&castrated=${castrated}&vaccinated=${vaccinated}&dewormed=${dewormed}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       updatePetList(data);
+//     })
+//     .catch((error) => console.error("Erro ao aplicar filtros:", error));
+// }
 
-function updatePetList(pets) {
-  const listContainer = document.querySelector(".list");
-  if (!listContainer) return;
+// function updatePetList(pets) {
+//   const listContainer = document.querySelector(".list");
+//   if (!listContainer) return;
 
-  listContainer.innerHTML = "";
+//   listContainer.innerHTML = "";
 
-  pets?.forEach((pet) => {
-    listContainer.innerHTML += `
-      <div class="list__item">
-        <div class="list__item__image">
-          <img src="${pet.image}" alt="Imagem do pet" />
-        </div>
-        <div class="list__item__header">
-          <h2>${pet.name}</h2>
-          <div class="list__item__header--right">
-            <!-- Adicione aqui lógica para verificar se o usuário é 'USER-DEFAULT' -->
-            <i class="ph-fill ph-heart ${
-              pet.isFavorite ? "favorite" : ""
-            }" onclick="addToFavorites(${pet.id})"></i>
-          </div>
-        </div>
-        <div class="list__item__description">
-          <p><b>Idade:</b> ${pet.age} anos</p>
-          <p><b>Sexo:</b> ${pet.gender}</p>
-        </div>
-        <div class="list__item__tags">
-          ${pet.castrated ? "<span>Castrado</span>" : ""}
-          ${pet.vaccinated ? "<span>Vacinado</span>" : ""}
-          ${pet.dewormed ? "<span>Vermificado</span>" : ""}
-        </div>
-      </div>
-    `;
-  });
-}
+//   pets?.forEach((pet) => {
+//     listContainer.innerHTML += `
+//       <div class="list__item">
+//         <div class="list__item__image">
+//           <img src="${pet.image}" alt="Imagem do pet" />
+//         </div>
+//         <div class="list__item__header">
+//           <h2>${pet.name}</h2>
+//           <div class="list__item__header--right">
+//             <!-- Adicione aqui lógica para verificar se o usuário é 'USER-DEFAULT' -->
+//             <i class="ph-fill ph-heart ${
+//               pet.isFavorite ? "favorite" : ""
+//             }" onclick="addToFavorites(${pet.id})"></i>
+//           </div>
+//         </div>
+//         <div class="list__item__description">
+//           <p><b>Idade:</b> ${pet.age} anos</p>
+//           <p><b>Sexo:</b> ${pet.gender}</p>
+//         </div>
+//         <div class="list__item__tags">
+//           ${pet.castrated ? "<span>Castrado</span>" : ""}
+//           ${pet.vaccinated ? "<span>Vacinado</span>" : ""}
+//           ${pet.dewormed ? "<span>Vermificado</span>" : ""}
+//         </div>
+//       </div>
+//     `;
+//   });
+// }
